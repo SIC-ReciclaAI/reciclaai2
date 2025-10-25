6fd0adfa8bb0129a4f9aafd5b4fd105cd45a0c76
Davi Patricio <davipatricio2006@gmail.com>
2025-10-25 10:22:38 -0300

    fix: open selector on button click

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Camera } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import ExampleImagesSection from '@/features/landing-page/sections/example-images';
 import FeaturesSection from '@/features/landing-page/sections/features';
 import HeroSection from '@/features/landing-page/sections/hero';
@@ -16,10 +18,18 @@ export default function Home() {
           {/* Mock image preview */}
           <Camera className="h-12 w-12 text-primary/60" />
         </div>
-        <Button className="mb-2 flex w-full items-center gap-2 font-semibold" size="lg" variant="default">
-          <Camera className="h-4 w-4" /> Enviar foto
-        </Button>
-        <p className="mb-2 text-foreground/60 text-sm">O processo de classificação pode demorar até 5 minutos.</p>
+
+        <Label className="mb-2 w-full cursor-pointer" htmlFor="image-upload">
+          <Button asChild className="flex w-full items-center gap-2 font-semibold" size="lg" variant="default">
+            <span>
+              <Camera className="h-4 w-4" /> Enviar foto
+            </span>
+          </Button>
+        </Label>
+
+        <Input accept="image/*" className="sr-only" id="image-upload" name="image-upload" type="file" />
+
+        <p className="text-foreground/60 text-sm">O processo de classificação pode demorar até 5 minutos.</p>
       </Card>
 
       <ExampleImagesSection />
